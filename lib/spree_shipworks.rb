98cb22bda18fa497b8a989d04735ea2039a1c13f
Szabolcs Pap <spap26@gmail.com>
2013-11-21 12:22:45 -0500

    Refactor order_id adjustment for preorders

--- lib/spree_shipworks.rb
+++ lib/spree_shipworks.rb
@@ -3,4 +3,5 @@ require 'spree_shipworks/engine'
 require 'spree_shipworks/dsl'
 
 module SpreeShipworks
+  PREORDER_ORDER_ID_ADJUSTMENT = 30000000
 end
--- lib/spree_shipworks/xml.rb
+++ lib/spree_shipworks/xml.rb
@@ -101,7 +101,7 @@ module SpreeShipworks
     module Order
       def to_shipworks_xml(context, preorder = false)
         context.element 'Order' do |order_context|
-          new_order_id = preorder ? (30000000 + self.id) : self.id
+          new_order_id = preorder ? (SpreeShipworks::PREORDER_ORDER_ID_ADJUSTMENT + self.id) : self.id
           order_context.element 'OrderNumber',    new_order_id
           order_context.element 'OrderDate',      self.created_at.to_s(:db).gsub(" ", "T")
           order_context.element 'LastModified',   self.updated_at.to_s(:db).gsub(" ", "T")
