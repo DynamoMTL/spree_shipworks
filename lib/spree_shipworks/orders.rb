229b35d3d8066df244cfa83a403b9042d210b7e6
Bryan Mahoney <bryan@goDynamo.com>
2014-06-19 09:59:30 -0400

    Revert "Ensures batch size isn't affected by orders skipped for gift cards"
    This reverts commit a003db7c0aa78f62cdcceb089c60bd90027d5db6.

--- lib/spree_shipworks/orders.rb
+++ lib/spree_shipworks/orders.rb
@@ -47,10 +47,7 @@ module SpreeShipworks
         orders = relation.offset(batch_size * batch).all
         while orders.any?
           orders.each do |order|
-            if order.only_contains_gift_cards?
-              counter += 1
-              next
-            end
+            next if order.only_contains_gift_cards?
             counter += 1
             if counter > batch_size && last_updated_at != order.updated_at
               broken = true
